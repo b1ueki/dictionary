cfea6275fd60e1e2a5e85d6e37e563303c5c63cf
岩田和俊 <k4z@iwatakazutoshinoMacBook-Pro.local>
2017-06-02 10:14:47 +0900

    upgrade

--- dictionary.c
+++ dictionary.c
@@ -6,23 +6,24 @@
 
 FILE *fp;
 
-char pass[];
-char word[];
+static char pass[64];
+static char word[64];
 
 int main(){
 
-  fp = fopen("lower","r");
-  printf("input pass > ");
+  fp = fopen("words","r");
+  printf("please input password > ");
   scanf("%s", pass);
 
   while(fgets(word, N, fp) != NULL){
-//    printf("%s\n", word);
-//    printf("%d\n",strlen(word));
+    printf("%s\n", word);
+    word[strlen(word) - 1] = '\0';
     if(!strcmp(word, pass)){
+      printf("----------------\n");
       printf("pass is [%s]\n", word);
+      printf("----------------\n");
       exit(1);
     }
-  //  memset(word, 0, strlen(word));
   }
 
   return 0;
